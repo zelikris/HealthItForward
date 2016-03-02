77a5e4b391241ef70abd6498ab7bca59379219d7
Siddu Duddikunta <siddu@siddu.me>
2016-03-02 13:36:47 -0500

    don't use debug by default

--- www/main.py
+++ www/main.py
@@ -24,7 +24,7 @@ def before_request():
 
 @app.after_request
 def response_minify(response):
-    if response.content_type == u'text/html; charset=utf-8':
+    if not app.debug and response.content_type == u'text/html; charset=utf-8':
         response.set_data(minify(response.get_data(as_text=True)))
     return response
 
@@ -134,4 +134,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
